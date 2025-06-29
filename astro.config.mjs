4bf1a303581ea685e097ae79999eea704085a95a
rendezvous <conestkawakami@gmail.com>
2025-06-29 19:57:01 +0900

    サイト設定を追加して404エラーを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://verdant-pasca-515ac4.netlify.app'
+});
